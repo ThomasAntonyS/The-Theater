96cae097ce99cd2371568b68b8dd32c00bda250a
Thomas Antony S <srtomsam@gmail.com>
2025-09-05 23:27:18 +0530

    fixed video component conditional rendering

--- src/Components/MovieHeroSection.jsx
+++ src/Components/MovieHeroSection.jsx
@@ -27,24 +27,24 @@ const MovieHeroSection = ({ item, handleWatchlist }) => {
 
       <div className="absolute inset-0 bg-gradient-to-t from-black via-black/50 to-transparent z-0" />
 
-      <div className="absolute bottom-5 w-full z-10 flex flex-col px-4 sm:px-6 md:px-12 lg:px-16 ">
+      <div className="absolute bottom-5 w-full z-10 flex flex-col px-4 sm:px-6 md:px-12 lg:px-14 ">
         <div className="flex flex-col flex-grow">
           <div className="flex flex-col sm:justify-between gap-4">
             <h2 className="font-manrope text-3xl sm:text-4xl md:text-5xl font-extrabold drop-shadow text-left">
               {item.original_title || item.title}
             </h2>
-            <h3 className=" text-lg sm:text-xl md:text-2xl font-nunito  line-clamp-2">{item.overview}</h3>
+            <h3 className=" text-md sm:text-lg md:text-xl font-nunito  line-clamp-2">{item.overview}</h3>
           </div>
         </div>
 
-        <div className="mt-5 py-4 text-gray-300 text-sm rounded-md">
+        <div className="mt-3 py-2 text-gray-300 text-sm rounded-md">
           <div className="flex flex-col md:flex-row md:justify-between gap-4">
-            <div className="flex flex-wrap text-lg sm:text-xl md:text-2xl align-middle gap-4 font-nunito">
+            <div className="flex flex-wrap text-md sm:text-lg md:text-xl align-middle gap-4 font-nunito">
               <p className="h-max my-auto">
                 <span className="text-red-500 font-semibold">Release Date:</span>{' '}
                 {formatDate(item.release_date)}
               </p>
-              <p className="h-max my-auto text-lg sm:text-xl md:text-2xl">
+              <p className="h-max my-auto text-md sm:text-lg md:text-xl">
                 <span className="text-red-500 font-semibold">Rating:</span>{' '}
                 <span className="text-yellow-400">{item.vote_average?.toFixed(1)}</span>
               </p>
@@ -53,7 +53,7 @@ const MovieHeroSection = ({ item, handleWatchlist }) => {
             <button
               onClick={handleWatchlist}
               title='Add to your watchlist'
-              className="flex h-max w-max my-auto text-lg sm:text-xl md:text-2xl bg-red-600 hover:bg-red-700 font-nunito text-white px-4 py-2 rounded-md sm:w-max"
+              className="flex h-max w-max my-auto text-md sm:text-lg md:text-xl bg-red-600 hover:bg-red-700 font-nunito text-white px-4 py-2 rounded-md sm:w-max"
             >
               <span className='h-max my-auto mx-2'><RiMovieLine size={23} /></span>Add to Watchlist 
             </button>
--- src/Components/Video.jsx
+++ src/Components/Video.jsx
@@ -1,13 +1,11 @@
 import { useEffect, useState } from 'react';
 import { Tailspin } from 'ldrs/react';
-import WestIcon from '@mui/icons-material/West';
-import EastIcon from '@mui/icons-material/East';
 import { East, West } from '@mui/icons-material';
 
 const Video = ({ id }) => {
-  const [videoData, setVideoData] = useState(null);
+  const [videoData, setVideoData] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [error, setError] = useState(false);
 
   const API_KEY = import.meta.env.VITE_API_KEY;
   const TMDB_URL = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}&language=en-US`;
@@ -16,15 +14,11 @@ const Video = ({ id }) => {
     const fetchVideoData = async () => {
       try {
         const response = await fetch(TMDB_URL);
-        if (!response.ok) {
-          throw new Error('Failed to fetch video data');
-        }
         const data = await response.json();
-        
         const trailers = data.results.filter(video => video.type === 'Trailer');
         const otherVideos = data.results.filter(video => video.type !== 'Trailer');
         
-        setVideoData([...trailers.slice(0, 1), ...otherVideos.slice(0, 14), ...trailers.slice(1)]);
+        setVideoData(()=>[...trailers.slice(0, 1), ...otherVideos.slice(0, 14), ...trailers.slice(1)]);
         setLoading(false);
       } catch (err) {
         setError(err.message);
@@ -35,43 +29,43 @@ const Video = ({ id }) => {
     fetchVideoData();
   }, [id]);
 
-  if (loading) {
-    return (
-      <div className="w-full h-screen mt-[10vh] flex items-start justify-center bg-black">
-        <Tailspin size={50} stroke={5} speed={0.9} color="white" />
-      </div>
-    );
-  }
 
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-
-  if (!videoData || videoData.length === 0) {
-    return null;
-  }
 
   return (
-    <div className="relative mb-16 w-full px-4 sm:px-8 lg:px-12 mx-auto" data-aos="fade-right" data-aos-duration="1000">
-      <p className="text-white text-2xl font-manrope sm:text-3xl md:text-4xl mb-6">Official Trailer & More</p>
-      <div className="flex overflow-x-auto space-x-4 no-scrollbar">
-        {videoData.map((video) => (
-          <div key={video.id} className="flex-shrink-0 w-full sm:w-1/2 md:w-1/2 xl:w-1/4">
-            <iframe
-              width="100%"
-              height="200"
-              src={`https://www.youtube.com/embed/${video.key}`}
-              title={video.name}
-              frameBorder="0"
-              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-              className="rounded-lg  h-[23vh] "
-            ></iframe>
-          </div>
-        ))}
+    <>
+    {videoData.length != 0 ?
+      <div className="relative mb-16 w-full px-4 sm:px-8 lg:px-12 mx-auto" data-aos="fade-right" data-aos-duration="1000">
+        <p className="text-white text-2xl font-manrope sm:text-3xl md:text-4xl mb-6">Official Trailer & More</p>
+        <div className="flex overflow-x-auto space-x-4 no-scrollbar">
+          {loading?
+            <div className="w-full h-screen mt-[10vh] flex items-start justify-center bg-black">
+              <Tailspin size={50} stroke={5} speed={0.9} color="white" />
+            </div>
+            :
+            <>
+            {videoData.map((video) => (
+              <div key={video.id} className="flex-shrink-0 w-full sm:w-1/2 md:w-1/2 xl:w-1/4">
+                <iframe
+                  width="100%"
+                  height="200"
+                  src={`https://www.youtube.com/embed/${video.key}`}
+                  title={video.name}
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                  className="rounded-lg  h-[30vh] xl:h-[25vh] "
+                ></iframe>
+              </div>
+            ))}
+            </>
+          }
+        </div>
+        <p className=' font-manrope w-max mx-auto mt-4'><West style={{marginRight:"10px"}}/> Swipe <East style={{marginLeft:"10px"}}/></p>
       </div>
-      <p className=' font-manrope w-max mx-auto mt-4'><West style={{marginRight:"10px"}}/> Swipe <East style={{marginLeft:"10px"}}/></p>
-    </div>
+      :
+      null
+    }
+    </>
   );
 };
 
